165d0e4f1ea64fea484af5643f88fceec64dcc96
David Amirkhanov <mail.websofter@gmail.com>
2024-02-15 14:20:34 +0300

    translate comments to RU

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,6 +19,11 @@ let pusher = new Pusher("1d9ffac87de599c61283", { cluster: "ap2" });
  * Here we are using aave smart contract on polygon
  * https://mumbai.polygonscan.com/address/0xcC6114B983E4Ed2737E9BD3961c9924e6216c704
  * We will use this to deposit funds to protocol
+ * ---------------------------------------------
+ * Адрес смарт-контракта партнера
+ * Здесь мы используем смарт-контракт aave для полигона.
+ * https://mumbai.polygonscan.com/address/0xcC6114B983E4Ed2737E9BD3961c9924e6216c704
+ * Мы будем использовать это для внесения средств в протокол.
  */
 const SMART_CONTRACT_ADDRESS = "0xcC6114B983E4Ed2737E9BD3961c9924e6216c704";
 
@@ -26,6 +31,10 @@ const SMART_CONTRACT_ADDRESS = "0xcC6114B983E4Ed2737E9BD3961c9924e6216c704";
  * The address of the token which smart contract is going to receive
  * Here we are using WBTC on polygon
  * Transak will send this token address amount to the smart contract
+ * ---------------------------------------------
+ * Адрес токена, который получит смарт-контракт.
+ * Здесь мы используем WBTC на полигоне.
+ * Transak отправит сумму этого адреса токена в смарт-контракт.
  */
 const SOURCE_TOKEN = "0x2Fa2e7a6dEB7bb51B625336DBe1dA23511914a8A";
 
@@ -33,12 +42,20 @@ const SOURCE_TOKEN = "0x2Fa2e7a6dEB7bb51B625336DBe1dA23511914a8A";
  * The address of the user
  * We will use this address to deposit funds to protocol
  * CallData needs to be encoded using the ABI for this wallet address
+ * ---------------------------------------------
+ * Адрес пользователя
+ * Мы будем использовать этот адрес для внесения средств в протокол.
+ * CallData необходимо закодировать с использованием ABI для этого адреса кошелька.
  */
 const USER_WALLET_ADDRESS = "0xb21CB810816539d8371ef56A2E10731ADAed59DB";
 /**
  * Supply function calldata
  * We will use this calldata to deposit funds to protocol
  * Data needs to be encoded using the ABI of the smart contract
+ * ---------------------------------------------
+ * Поставка данных вызова функции
+ * Мы будем использовать эти данные вызова для внесения средств в протокол.
+ * Данные необходимо закодировать с использованием ABI смарт-контракта.
  */
 const getSupplyCalldata = (contributor, amount) => {
   if (!amount) return;
@@ -47,17 +64,21 @@ const getSupplyCalldata = (contributor, amount) => {
    * ABI for Supply function on aave SmartContract
    * You can find ABI of aave contract here
    * https://mumbai.polygonscan.com/address/0xbadd48c3eb42a10db791d7b02e3c07fbf95b3155#code
+   * ---------------------------------------------
+   * ABI для функции снабжения в aave SmartContract.
+   * Вы можете найти ABI контракта aave здесь.
+   * https://mumbai.polygonscan.com/address/0xbadd48c3eb42a10db791d7b02e3c07fbf95b3155#code
    */
   let ABI = [
     "function supply(address asset,uint256 amount,address onBehalfOf,uint16 referralCode)",
   ];
   let parsedAmmount = parseUnits(amount.toString(), 8);
-  // Constructing the CallData
+  // Constructing the CallData / Создание данных вызова
   return new Interface(ABI).encodeFunctionData("supply", [
-    SOURCE_TOKEN, // contract address for WBTC on mumbai testnet
-    parsedAmmount, // amount user wants to buy
-    contributor, // user who is depositing the funds
-    0, // referal code. Will be 0 in case of aave
+    SOURCE_TOKEN, // contract address for WBTC on mumbai testnet / Контрактный адрес WBTC в тестовой сети Мумбаи
+    parsedAmmount, // amount user wants to buy / сумма, которую пользователь хочет купить
+    contributor, // user who is depositing the funds / пользователь, который вносит средства
+    0, // referal code. Will be 0 in case of aave / Промо-код. Будет 0 в случае aave
   ]);
 };
 
@@ -68,17 +89,21 @@ export default {
   },
   setup() {
     watchEffect(() => {
-      const depositAmount = 0.0008; // amount user want to deposit to protocol
+      const depositAmount = 0.0008; // amount user want to deposit to protocol / сумма, которую пользователь хочет внести в протокол
 
       const calldata = getSupplyCalldata(
-        USER_WALLET_ADDRESS, // user wallet address
+        USER_WALLET_ADDRESS, // user wallet address / адрес кошелька пользователя
         depositAmount, // amount user want to deposit. Transak will convert it to the fiat equivalent and display it to the user
+        // сумма, которую пользователь хочет внести. Transak преобразует его в фиатный эквивалент и отобразит пользователю.
       );
 
       if (!calldata) return;
 
       /**
        * You can find list of all possible Transak config options here
+       * https://docs.transak.com/docs/transak-one-query-parameters
+       * ------------------------------------
+       * Список всех возможных вариантов конфигурации Transak можно найти здесь.
        * https://docs.transak.com/docs/transak-one-query-parameters
        */
       const settings = {
@@ -89,7 +114,9 @@ export default {
         /**
          * Wallet address of the user
          * The blockchain address of the user's wallet that the receipt token will be sent to.
-         *
+         * ----------------------------------------------------
+         * Адрес кошелька пользователя
+         * Адрес блокчейна кошелька пользователя, на который будет отправлен токен квитанции.
          */
         walletAddress: USER_WALLET_ADDRESS,
         exchangeScreenTitle: "Deposit Funds",
@@ -102,6 +129,11 @@ export default {
          * in the transaction we are supplying WBTC to Aave protocol
          * So we are sending WBTC as sourceTokenData along with the amount
          * we want to deposit on users behalf
+         * -----------------------------------------------------
+         * Подробная информация о смарт-контракте токена, который будет использоваться.
+         * в транзакции мы передаем WBTC по протоколу Aave
+         * Итак, мы отправляем WBTC как sourceTokenData вместе с суммой.
+         * мы хотим внести депозит от имени пользователей
          */
         sourceTokenData: [
           {
@@ -115,6 +147,12 @@ export default {
          * So in this case we are sending AWBTC as cryptoCurrencyData
          * Along with name and image of the crypto currency.
          * This will be shown to the user.
+         * ---------------------------------------------------------
+         * Подробная информация о криптопользователе будет получена после совершения транзакции.
+         * Например, если вы хотите внести WBTC, взамен вы получите AWBTC.
+         * В данном случае мы отправляем AWBTC как cryptoCurrencyData.
+         * Вместе с названием и изображением криптовалюты.
+         * Это будет показано пользователю.
          */
         cryptoCurrencyData: [
           {
@@ -134,12 +172,12 @@ export default {
       const subscribeToWebsockets = (orderId) => {
         let channel = pusher.subscribe(orderId);
 
-        //receive updates of all the events
+        // receive updates of all the events / получать обновления обо всех событиях
         pusher.bind_global((eventId, orderData) => {
           console.log(`websocket Event: ${eventId} with order data:`, orderData);
         });
 
-        //receive updates of a specific event
+        // receive updates of a specific event / получать обновления о конкретном событии
         channel.bind("ORDER_COMPLETED", (orderData) => {
           console.log("ORDER COMPLETED websocket event", orderData);
         });
